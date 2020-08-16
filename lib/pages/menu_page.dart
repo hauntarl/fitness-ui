77c0faec1629277880cd69c0fb62fa12c479180c
hauntarl <sameer.mungole@gmail.com>
2020-08-16 23:47:50 +0530

    further optimizations for bar graph loading

--- lib/pages/menu_page.dart
+++ lib/pages/menu_page.dart
@@ -44,7 +44,13 @@ class MenuPage extends StatelessWidget {
                   _sizedBox20,
                   _buildTimeline,
                   _sizedBox20,
-                  _generateBarGraph,
+                  FutureBuilder(
+                    future: _generateBarGraph,
+                    builder: (_, snapshot) =>
+                        snapshot.connectionState == ConnectionState.waiting
+                            ? Container()
+                            : snapshot.data,
+                  ),
                   _sizedBox20,
                   _buildFilter,
                 ],
@@ -56,143 +62,7 @@ class MenuPage extends StatelessWidget {
     );
   }
 
-  static const _sizedBox30 = SizedBox(height: 30);
-  static const _sizedBox20 = SizedBox(height: 20);
-
-  Widget get _buildTitle {
-    return Text(
-      'ACTIVITIES',
-      style: Styles.title.copyWith(
-        color: Colors.black54,
-        letterSpacing: 1,
-        fontWeight: FontWeight.w900,
-      ),
-      textAlign: TextAlign.center,
-    );
-  }
-
-  Widget get _row1 {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        ActivityCard(
-          activity: 'Swimming',
-          background: ColorPalette.red,
-          imagePath: 'assets/images/swimming.PNG',
-        ),
-        ActivityCard(
-          activity: 'Cycling',
-          background: ColorPalette.teal,
-          imagePath: 'assets/images/cycling.PNG',
-        ),
-        ActivityCard(
-          activity: 'Running',
-          background: ColorPalette.blue,
-          imagePath: 'assets/images/running.PNG',
-        ),
-      ],
-    );
-  }
-
-  Widget get _row2 {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        ActivityCard(
-          activity: 'Yoga',
-          background: ColorPalette.purple,
-          imagePath: 'assets/images/yoga.PNG',
-        ),
-        ActivityCard(
-          activity: 'Dance',
-          background: ColorPalette.activityPurple,
-          imagePath: 'assets/images/dance.PNG',
-        ),
-        ActivityCard(
-          activity: 'Gym',
-          background: ColorPalette.cyan,
-          imagePath: 'assets/images/gym.PNG',
-        ),
-      ],
-    );
-  }
-
-  Widget get _buildActivityStats {
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 15),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                '4865',
-                style: TextStyle(
-                  fontSize: 25,
-                  fontWeight: FontWeight.w500,
-                  color: Colors.black.withOpacity(0.7),
-                ),
-              ),
-              Text(
-                'Steps',
-                style: TextStyle(
-                  color: Colors.black38,
-                  fontSize: 16,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-            ],
-          ),
-          Spacer(),
-          Text(
-            'Running',
-            style: TextStyle(
-              color: ColorPalette.purple,
-              fontSize: 15,
-              letterSpacing: 1.1,
-            ),
-          ),
-          SizedBox(width: 5),
-          Icon(
-            Icons.directions_run,
-            color: ColorPalette.purple,
-            size: 18,
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildText(String data, [bool highlight = false]) {
-    return Text(
-      data,
-      style: TextStyle(
-        color: highlight ? Colors.black : Colors.black26,
-        fontWeight: FontWeight.w600,
-        fontSize: 16,
-      ),
-    );
-  }
-
-  Widget get _buildTimeline {
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 15),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          _buildText('3am'),
-          _buildText('6am'),
-          _buildText('7am'),
-          _buildText('12pm'),
-          _buildText('15pm'),
-          _buildText('18pm', true),
-        ],
-      ),
-    );
-  }
-
-  Widget get _generateBarGraph {
+  Future<Widget> get _generateBarGraph async {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: List.generate(15, (_) {
@@ -206,17 +76,141 @@ class MenuPage extends StatelessWidget {
     );
   }
 
-  Widget get _buildFilter {
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 15),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          _buildText('Day', true),
-          _buildText('Week'),
-          _buildText('Month'),
-        ],
+  static const _sizedBox30 = SizedBox(height: 30);
+  static const _sizedBox20 = SizedBox(height: 20);
+
+  static final Widget _buildTitle = Text(
+    'ACTIVITIES',
+    style: Styles.title.copyWith(
+      color: Colors.black54,
+      letterSpacing: 1,
+      fontWeight: FontWeight.w900,
+    ),
+    textAlign: TextAlign.center,
+  );
+
+  static final Widget _row1 = Row(
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: [
+      ActivityCard(
+        activity: 'Swimming',
+        background: ColorPalette.red,
+        imagePath: 'assets/images/swimming.PNG',
+      ),
+      ActivityCard(
+        activity: 'Cycling',
+        background: ColorPalette.teal,
+        imagePath: 'assets/images/cycling.PNG',
+      ),
+      ActivityCard(
+        activity: 'Running',
+        background: ColorPalette.blue,
+        imagePath: 'assets/images/running.PNG',
+      ),
+    ],
+  );
+
+  static final Widget _row2 = Row(
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: [
+      ActivityCard(
+        activity: 'Yoga',
+        background: ColorPalette.purple,
+        imagePath: 'assets/images/yoga.PNG',
+      ),
+      ActivityCard(
+        activity: 'Dance',
+        background: ColorPalette.activityPurple,
+        imagePath: 'assets/images/dance.PNG',
+      ),
+      ActivityCard(
+        activity: 'Gym',
+        background: ColorPalette.cyan,
+        imagePath: 'assets/images/gym.PNG',
+      ),
+    ],
+  );
+
+  static final Widget _buildActivityStats = Padding(
+    padding: EdgeInsets.symmetric(horizontal: 15),
+    child: Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              '4865',
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.w500,
+                color: Colors.black.withOpacity(0.7),
+              ),
+            ),
+            Text(
+              'Steps',
+              style: TextStyle(
+                color: Colors.black38,
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ],
+        ),
+        Spacer(),
+        Text(
+          'Running',
+          style: TextStyle(
+            color: ColorPalette.purple,
+            fontSize: 15,
+            letterSpacing: 1.1,
+          ),
+        ),
+        SizedBox(width: 5),
+        Icon(
+          Icons.directions_run,
+          color: ColorPalette.purple,
+          size: 18,
+        ),
+      ],
+    ),
+  );
+
+  static Widget _buildText(String data, [bool highlight = false]) {
+    return Text(
+      data,
+      style: TextStyle(
+        color: highlight ? Colors.black : Colors.black26,
+        fontWeight: FontWeight.w600,
+        fontSize: 16,
       ),
     );
   }
+
+  static final Widget _buildTimeline = Padding(
+    padding: EdgeInsets.symmetric(horizontal: 15),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        _buildText('3am'),
+        _buildText('6am'),
+        _buildText('7am'),
+        _buildText('12pm'),
+        _buildText('15pm'),
+        _buildText('18pm', true),
+      ],
+    ),
+  );
+
+  static final Widget _buildFilter = Padding(
+    padding: EdgeInsets.symmetric(horizontal: 15),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        _buildText('Day', true),
+        _buildText('Week'),
+        _buildText('Month'),
+      ],
+    ),
+  );
 }
